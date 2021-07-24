f0d0d5f5273f13bbb02e50520be706ac483c4689
chieping <takashi.honda.01@gmail.com>
2021-07-25 01:51:12 +0900

    revise

--- abc211/c/main.py
+++ abc211/c/main.py
@@ -1,27 +1,14 @@
 MOD = 1_000_000_007
-S = input()
+s = input()
+n = len(s)
+cho = 'chokudai'
 
-cnt = [0] * 8
+dp = [0] * (len(cho) + 1)
+dp[0] = 1
 
-for s in S:
-    if s == 'c':
-        cnt[0] += 1
-    elif s == 'h':
-        cnt[1] += cnt[0]
-    elif s == 'o':
-        cnt[2] += cnt[1]
-    elif s == 'k':
-        cnt[3] += cnt[2]
-    elif s == 'u':
-        cnt[4] += cnt[3]
-    elif s == 'd':
-        cnt[5] += cnt[4]
-    elif s == 'a':
-        cnt[6] += cnt[5]
-    elif s == 'i':
-        cnt[7] += cnt[6]
+for i in range(n):
+    for j in range(len(cho)):
+        if s[i] == cho[j]:
+            dp[j+1] = (dp[j+1] + dp[j]) % MOD
 
-print(cnt[7] % MOD)
-
-
-
+print(dp[8])
