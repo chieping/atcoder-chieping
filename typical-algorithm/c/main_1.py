45686881390990d0ae57afd162dfc475df0b2d7c
chieping <takashi.honda.01@gmail.com>
2021-08-15 12:53:39 +0900

    typ

--- typical-algorithm/c/main_1.py
+++ typical-algorithm/c/main_1.py
@@ -1,10 +1,9 @@
-N = int(input())
+N  = int(input())
 A = [list(map(int, input().split())) for i in range(N)]
 
-ALL = 1 << N
+ALL = 1<<N
 
-# cost[N: 訪れた都市の集合][i: 最後にいる都市] = コスト最小値
-cost = [[10**100]*N for i in range(ALL)]
+cost = [[10**100] * N for i in range(ALL)]
 
 cost[0][0] = 0
 
