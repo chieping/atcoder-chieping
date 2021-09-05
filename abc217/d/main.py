276692fbdfd513ac45e8f7842d83c4e744db594e
chieping <takashi.honda.01@gmail.com>
2021-09-05 09:00:55 +0900

    abc217_d

--- abc217/d/main.py
+++ abc217/d/main.py
@@ -1,16 +1,14 @@
+import array
 from bisect import bisect_left, insort_left
 
 L, Q = map(int, input().split())
-K = [0, L]
+K = array.array('i', [0, L])
 
 for _ in range(Q):
     c, x = map(int, input().split())
 
     if c == 1:
-        # insort_left(K, x)
-        j = bisect_left(K, x)
-        K.insert(j, x)
-
+        insort_left(K, x)
     else:
         j = bisect_left(K, x)
         print(K[j] - K[j-1])
