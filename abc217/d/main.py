c663845e67926745722cc5b2c3c9ecbd35660cf3
chieping <takashi.honda.01@gmail.com>
2021-09-05 09:30:40 +0900

    abc217_d_using_sortedcontainers

--- abc217/d/main.py
+++ abc217/d/main.py
@@ -1,14 +1,16 @@
-import array
-from bisect import bisect_left, insort_left
+# AtCoder上ではimportできないので提出する際はコードを貼り付ける
+# SortedSetはSortedListを利用するので貼り付ける手間が多いのでSortedListを使用した
+from sortedcontainers import SortedList
 
 L, Q = map(int, input().split())
-K = array.array('i', [0, L])
+
+st = SortedList([0, L])
 
 for _ in range(Q):
     c, x = map(int, input().split())
 
     if c == 1:
-        insort_left(K, x)
+        st.add(x)
     else:
-        j = bisect_left(K, x)
-        print(K[j] - K[j-1])
+        i = st.bisect_left(x)
+        print(st[i] - st[i-1])
