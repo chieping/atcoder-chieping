6bd6311ae33603c08687d785d626ac8bd66c0eac
chieping <takashi.honda.01@gmail.com>
2021-09-14 09:07:26 +0900

    update

--- tdpc_a/tdpc/a/main.py
+++ tdpc_a/tdpc/a/main.py
@@ -1,19 +1,14 @@
 N = int(input())
 P = list(map(int, input().split()))
 
-dp = [[False] * 10100 for i in range(N + 1)]
+dp = [[False] * (N * 100) for i in range(N + 1)]
 
 dp[0][0] = True
 
 for i in range(1, N + 1):
-    for j in range(10100):
+    for j in range(N * 100):
         dp[i][j] = dp[i-1][j]
         if j - P[i-1] >= 0:
             dp[i][j] |= dp[i-1][j-(P[i-1])]    
 
-ans = 0
-for i in range(10100):
-    if dp[N][i]:
-        ans += 1
-
-print(ans)
+print(sum(dp[N]))
