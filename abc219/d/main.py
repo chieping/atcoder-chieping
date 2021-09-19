8c4ae2dab6499ab7179b0babb9b0d78de917f9bd
chieping <takashi.honda.01@gmail.com>
2021-09-19 09:51:12 +0900

    abc219_d

--- abc219/d/main.py
+++ abc219/d/main.py
@@ -2,46 +2,24 @@ from collections import defaultdict
 INF = 10**18
 N = int(input())
 X, Y = map(int, input().split())
-bentos = [list(map(int, input().split())) for i in range(N)]
+B = [list(map(int, input().split())) for i in range(N)]
 
-# choice[i][(tako, tai)] = 選んだ個数
-choice = [{} for i in range(N+1)]
-choice[0][(0, 0)] = 0
+# dp[i][j][k] = 最小の弁当個数
+dp = [[[INF] * (Y+1) for i in range(X+1)] for i in range(N+1)]
 
-for i in range(N):
-    a, b = bentos[i]
-    for tako in range(300):
-        for tai in range(300):
-            if (tako, tai) in choice[i]:
-                k0 = choice[i][(tako, tai)]
-            else:
-                k0 = INF
-            if (tako, tai) in choice[i+1]:
-                k1 = choice[i+1][(tako, tai)]
-            else:
-                k1 = INF
-            if (tako - a, tai - b) in choice[i]:
-                k3 = choice[i][(tako - a, tai - b)]
-            else:
-                k3 = INF
+dp[0][0][0] = 0
 
-            # 買わない場合
-            # choice[i+1][(tako, tai)] = min(choice[i][(tako, tai)], choice[i+1][(tako, tai)])
-            choice[i+1][(tako, tai)] = min(k0, k1, k3 + 1)
+for i in range(1, N + 1):
+    tako = B[i-1][0]
+    tai = B[i-1][1]
+    for j in range(X+1):
+        for k in range(Y+1):
             # 買う場合
-            # choice[i+1][(tako, tai)] = min(choice[i+1][(tako, tai)], choice[i][(tako - a, tai - b)] + 1)
-            # choice[i+1][(tako, tai)] = min(k1, k3 + 1)
+            dp[i][min(j + tako, X)][min(k + tai, Y)] = min(dp[i][min(j + tako, X)][min(k + tai, Y)], dp[i-1][j][k] + 1)
+            # 買わない場合
+            dp[i][j][k] = min(dp[i][j][k], dp[i-1][j][k])
 
-ans = INF
-for (a, b) in choice[N]:
-    if a >= X and b >= Y:
-        ans = min(ans, choice[N][(a, b)])
-
-if ans == INF:
-    ans = -1
-
-print(ans)
-
-
-
-    
+if dp[N][X][Y] != INF:
+    print(dp[N][X][Y])
+else:
+    print(-1)
