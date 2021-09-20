1845f346aed5f124dbe6583be5cb88c4e9950474
chieping <takashi.honda.01@gmail.com>
2021-09-20 19:14:25 +0900

    abc197_c

--- abc197/c/main.py
+++ abc197/c/main.py
@@ -1,2 +1,14 @@
 N = int(input())
 A = list(map(int, input().split()))
+ans = 1<<60
+for bit in range(1<<(N-1)):
+    xored = 0
+    ored = 0
+    for i in range(N):
+        ored |= A[i]
+        if (bit >> i) & 1:
+            xored ^= ored
+            ored = 0
+    xored ^= ored
+    ans = min(ans, xored)
+print(ans)
