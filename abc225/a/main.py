c86db0fcaf424c1d8bca3d919182730f78c05206
chieping <takashi.honda.01@gmail.com>
2021-10-30 23:24:08 +0900

    update

--- abc225/a/main.py
+++ abc225/a/main.py
@@ -1,11 +1,2 @@
-S = list(input())
-st = set()
-
-st.add(S[0] + S[1] + S[2])
-st.add(S[0] + S[2] + S[1])
-st.add(S[1] + S[2] + S[0])
-st.add(S[1] + S[0] + S[2])
-st.add(S[2] + S[1] + S[0])
-st.add(S[2] + S[0] + S[1])
-
-print(len(st))
+from itertools import permutations
+print(len(set(permutations(list(input())))))
