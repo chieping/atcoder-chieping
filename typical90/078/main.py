b8b2bb6966d1419d861d95b4cbe3a7baf78e6ec4
chieping <takashi.honda.01@gmail.com>
2021-11-07 08:50:56 +0900

    typical90_078

---
+++ typical90/078/main.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+edges = [[] for i in range(N)]
+for i in range(M):
+    a, b = map(int, input().split())
+    a -= 1
+    b -= 1
+    edges[a].append(b)
+    edges[b].append(a)
+
+ans = 0
+for i in range(N):
+    if len(list(filter(lambda x: x < i, edges[i]))) == 1:
+        ans += 1
+print(ans)
