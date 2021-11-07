9106a3376bfc84ebf68bf3f9d9529afb4e22e177
chieping <takashi.honda.01@gmail.com>
2021-11-07 23:41:10 +0900

    Update UnionFind

---
+++ src/union_find.py
@@ -0,0 +1,30 @@
+class UnionFind:
+    def __init__(self, n: int) -> None:
+        self.parents = [-1] * n
+        self.data = [0] * n
+
+    def find(self, x: int) -> int:
+        if self.parents[x] < 0:
+            return x
+        self.parents[x] = self.find(self.parents[x])
+        return self.parents[x]
+
+    def union(self, x: int, y: int) -> None:
+        x = self.find(x)
+        y = self.find(y)
+        if x == y:
+            return
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+        self.union_hook(x, y)
+
+    def size(self, x: int) -> int:
+        return -self.parents[self.find(x)]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    def union_hook(self, x, y):
+        pass
