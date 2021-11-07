b3752677967830b76f56f6b7f0a5d66b9a6c117c
chieping <takashi.honda.01@gmail.com>
2021-11-07 09:00:35 +0900

    update

--- typical90/078/main.py
+++ typical90/078/main.py
@@ -9,6 +9,7 @@ for i in range(M):
 
 ans = 0
 for i in range(N):
-    if len(list(filter(lambda x: x < i, edges[i]))) == 1:
+    # if len(list(filter(lambda x: x < i, edges[i]))) == 1:
+    if len([x for x in edges[i] if x < i]) == 1:
         ans += 1
 print(ans)
