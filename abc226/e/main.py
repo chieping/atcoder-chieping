5de783032207ea412fb91dc2b67b2050f734ec97
chieping <takashi.honda.01@gmail.com>
2021-11-07 23:40:51 +0900

    abc226_e

--- abc226/e/main.py
+++ abc226/e/main.py
@@ -1,29 +1,35 @@
 class UnionFind:
     def __init__(self, n: int) -> None:
         self.parents = [-1] * n
+        self.data = [0] * n
 
     def find(self, x: int) -> int:
         if self.parents[x] < 0:
             return x
-        else:
-            self.parents[x] = self.find(self.parents[x])
-            return self.parents[x]
-    
-    def unite(self, x: int, y: int) -> None:
+        self.parents[x] = self.find(self.parents[x])
+        return self.parents[x]
+
+    def union(self, x: int, y: int) -> None:
         x = self.find(x)
         y = self.find(y)
-        if x == y:
-            return
-        if self.parents[x] > self.parents[y]:
-            x, y = y, x
-        self.parents[x] += self.parents[y]
-        self.parents[y] = x
-    
+        if x != y:
+            if self.parents[x] > self.parents[y]:
+                x, y = y, x
+            self.parents[x] += self.parents[y]
+            self.parents[y] = x
+        self.union_hook(x, y)
+
     def size(self, x: int) -> int:
         return -self.parents[self.find(x)]
 
     def roots(self):
         return [i for i, x in enumerate(self.parents) if x < 0]
+
+    def union_hook(self, x, y):
+        if x != y:
+            self.data[x] += self.data[y]
+            self.data[y] = 0
+        self.data[x] += 1
 
 
 MOD = 998244353
@@ -33,12 +39,12 @@ for i in range(M):
     u, v = map(int, input().split())
     u -= 1
     v -= 1
-    uf.unite(u, v)
+    uf.union(u, v)
 
 ans = 1
 
 for root in uf.roots():
-    if uf.size(root)>2:
+    if uf.size[root] == uf.data[root]:
         ans *= 2
         ans %= MOD
     else:
