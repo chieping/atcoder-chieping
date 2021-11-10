a8bf32cbf4958f6384d83ebbdf65fb30f1731498
chieping <takashi.honda.01@gmail.com>
2021-11-10 20:02:41 +0900

    update

--- abc193/c/main.py
+++ abc193/c/main.py
@@ -1,14 +1,10 @@
-from collections import defaultdict
 N = int(input())
-M = defaultdict(int)
-ans = 0
+S = set()
 i = 2
 while i**2 <= N:
     j = 2
     while i**j <= N:
-        if M[i**j] == 0:
-            M[i**j] = 1
-            ans += 1
+        S.add(i**j)
         j += 1
     i += 1
-print(N-ans)
+print(N-len(S))
