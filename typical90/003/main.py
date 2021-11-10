0fbfc304c6de975efdd105191979bc3f35bd4e52
chieping <takashi.honda.01@gmail.com>
2021-11-10 19:14:54 +0900

    update

--- typical90/003/main.py
+++ typical90/003/main.py
@@ -9,30 +9,26 @@ for i in range(N-1):
     edges[a-1].append(b-1)
     edges[b-1].append(a-1)
 
+def bfs(start):
+    dist = [False] * N
+    dist[start] = (0, 0)
+    Q = deque()
+    Q.append(start)
+    while len(Q) > 0:
+        v = Q.pop()
+        for i in edges[v]:
+            if dist[i] == False:
+                dist[i] = (dist[v][0] + 1, i)
+                Q.append(i)
+    return dist
+
 # 頂点1から各頂点までの最短距離を求める
-D0 = [False] * N
-D0[0] = (0, 0)
-Q = deque()
-Q.append(0)
-while len(Q) > 0:
-    v = Q.pop()
-    for i in edges[v]:
-        if D0[i] == False:
-            D0[i] = (D0[v][0] + 1, i)
-            Q.append(i)
+D0 = bfs(0)
 
 # 最も最短距離が大きかった頂点をuとして、頂点uからの最短距離を求める。
 # そのときに出現した最短距離最大値がE(木の直径)の値である
 u = max(D0)[1]
-DM = [False] * N
-DM[u] = (0, 0)
-Q.append(u)
-while len(Q) > 0:
-    v = Q.pop()
-    for i in edges[v]:
-        if DM[i] == False:
-            DM[i] = (DM[v][0] + 1, i)
-            Q.append(i)
+DM = bfs(u)
 
 ans = max(DM)[0] + 1
 print(ans)
