39aec301a35bd1d771681dd91a9913dd31717892
chieping <takashi.honda.01@gmail.com>
2021-11-13 11:00:18 +0900

    update

--- typical90/008/main.py
+++ typical90/008/main.py
@@ -3,7 +3,6 @@ S = ["X"] + list(input())
 atcoder = "Xatcoder"
 MOD = 10**9+7
 dp = [[1] + [0] * 7 for i in range(N+1)]
-dp[0][0] = 1
 for i in range(1, N+1):
     for j in range(1, 8):
         if S[i] == atcoder[j]:
