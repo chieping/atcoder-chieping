c2ea2dd2fc437191b3d02f18f5521a6e2e99effa
chieping <takashi.honda.01@gmail.com>
2021-11-22 08:33:57 +0900

    ac

--- abc228/d/main.py
+++ abc228/d/main.py
@@ -1,15 +1,34 @@
 MOD = 2**20
 Q = int(input())
-A = [-1] * (MOD)
-parent = [i for i in range(MOD)]
-for i in range(1, Q+1):
+A = [-1] * MOD
+P = [-1] * MOD
+
+def find(x):
+    # print('x:', x)
+    if P[x] < 0:
+        return x
+    else:
+        P[x] = find(P[x])
+        return P[x]
+
+def unite(x, y):
+    # print('x:', x, 'y:', y)
+    x = find(x)
+    y = find(y)
+    if x == y:
+        return
+    P[y] += P[x]
+    P[x] = y
+
+
+for _ in range(Q):
     t, x = map(int, input().split())
-    if (t == 1):
+    if t == 1:
         if A[x % MOD] == -1:
             h = x%MOD
         else:
-            h = parent[x % MOD]
+            h = find(x%MOD)
         A[h] = x
-        parent[h] = parent[(h+1)%MOD]
+        unite(h, (h+1)%MOD)
     elif t == 2:
         print(A[x % MOD])
