376d8dd439947ac7e366748e8396dd1e0c2145ae
chieping <takashi.honda.01@gmail.com>
2021-12-04 13:08:55 +0900

    abc230_d

--- abc230/d/main.py
+++ abc230/d/main.py
@@ -1,41 +1,15 @@
-from collections import Counter, OrderedDict
-
 N, D = map(int, input().split())
-
-imos = OrderedDict()
-# imos = [0] * (N+2)
-
-
-
-
+ans = 0
+walls = []
 for i in range(N):
     l, r = map(int, input().split())
-    if not l in imos:
-        imos[l] = 0
-    if not r in imos:
-        imos[r] = 0
-    imos[l] += 1
-    imos[r] -= 1
-
-ans = 0
-A = 0
-
-i = 0
-for k, v in sorted(imos.items()):
-    if A == 0 and v > 0:
-        i = k
-    A += v
-    if A == 0 and v < 0:
-        ans += k - i + 1
-        i = k
-    
-
-
-# for i in range(1, 10**9):
-#     A += imos[i]
-#     if A > 0:
-#         ans += 1
-
+    walls.append((r, l))
+# 最後に壊した列
+last = 0
+walls.sort()
+for r, l in walls:
+    if l <= last:
+        continue
+    last = r+D-1
+    ans += 1
 print(ans)
-
-
