47ba5d507c1583a3d11f6c1053c869fb68a3d765
chieping <takashi.honda.01@gmail.com>
2021-12-05 19:11:28 +0900

    update

--- arc122/b/main.py
+++ arc122/b/main.py
@@ -1,14 +1,12 @@
+import itertools
+
+
 N = int(input())
 A = list(map(int, input().split()))
 A.sort()
-acc = [A[0]]
-for i in range(N-1):
-    acc.append(acc[i] + A[i+1])
-
+acc = list(itertools.accumulate(A))
 ans = 10**18
-
 for i in range(N):
     x = A[i] / 2
     ans = min(ans, (x*N) + acc[N-1]-acc[i] - (x*2*(N-i-1)))
-
 print(ans/N)
