9b54776942be4dbf7d088768ebe5772872777b4a
chieping <takashi.honda.01@gmail.com>
2021-12-10 21:58:16 +0900

    db_b

--- dp/b/main.py
+++ dp/b/main.py
@@ -1,15 +1,10 @@
 N, K = map(int, input().split())
-h = list(map(int, input().split()))
-
-dp = [10**18] * N
+h = [0] + list(map(int, input().split()))
+dp = [10**18] * (N+1)
 dp[0] = 0
-
-for i in range(1, N):
-    m = 10**18
+dp[1] = 0
+for i in range(2, N+1):
     for j in range(1, K+1):
-        if i < j:
-            break
-        m = min(m, dp[i-j]+abs(h[i]-h[i-j]))
-    dp[i] = m
-
-print(dp[N-1])
+        if i-j > 0:
+            dp[i] = min(dp[i], dp[i-j] + abs(h[i]-h[i-j]))
+print(dp[N])
