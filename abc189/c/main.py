493afbc4ab8c33b6e246846a399c97ea0e6d2345
chieping <takashi.honda.01@gmail.com>
2021-12-18 00:01:19 +0900

    abc189_c

--- abc189/c/main.py
+++ abc189/c/main.py
@@ -1,3 +1,9 @@
 N = int(input())
 A = list(map(int, input().split()))
-
+l, r, ans = 0, 0, 0
+for l in range(N):
+    x = A[l]
+    for r in range(l, N):
+        x = min(x, A[r])
+        ans = max(ans, (r-l+1)*x)
+print(ans)
