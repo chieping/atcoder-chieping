bced968a0c278a074c13f2e0285d894a362591b0
chieping <takashi.honda.01@gmail.com>
2021-12-19 12:55:39 +0900

    dp_d

--- dp/d/main.py
+++ dp/d/main.py
@@ -1,27 +1,11 @@
+from collections import defaultdict
 N, W = map(int, input().split())
-
-dp = [[0] * N for i in range(N)]
-
-ws = [0]
-vs = [0]
-
-for i in range(N):
-    w, v = map(int, input().split())
-    ws.append(w)
-    vs.append(v)
-
-value = []
-for i in range(N+1):
-    value.append([-10**18]*(W+1))
-
-value[0][0] = 0
-
+dp = [defaultdict(int) for _ in range(N+1)]
+dp[0][0] = 0
 for i in range(1, N+1):
-    for w in range(W+1):
-        value[i][w] = max(value[i][w], value[i-1][w])
-        if w-ws[i] >= 0:
-            value[i][w] = max(value[i][w], value[i-1][w-ws[i]] + vs[i])
-
-print(max(value[N]))
-    
-
+    w, nv = map(int, input().split())
+    for k, cv in dp[i-1].items():
+        dp[i][k] = max(dp[i][k], cv)
+        if k+w <= W:
+            dp[i][k+w] = max(dp[i][k+w], cv+nv)
+print(max(dp[N].values()))
