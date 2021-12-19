af45ad49e8e09612e65e18303c68500397d52e8e
chieping <takashi.honda.01@gmail.com>
2021-12-19 12:21:51 +0900

    dp_c

--- dp/c/main.py
+++ dp/c/main.py
@@ -1,19 +1,8 @@
 N = int(input())
-
-dp = [[0] * 3 for i in range(N)]
-
-a, b, c = map(int, input().split())
-dp[0][0] = a
-dp[0][1] = b
-dp[0][2] = c
-
-for i in range(1, N):
+dp = [[0] * 3 for _ in range(N+1)]
+for i in range(1, N+1):
     a, b, c = map(int, input().split())
-
-    dp[i][0] = max(dp[i-1][1], dp[i-1][2]) + a
-    dp[i][1] = max(dp[i-1][0], dp[i-1][2]) + b
-    dp[i][2] = max(dp[i-1][0], dp[i-1][1]) + c
-
-print(max(dp[N-1]))
-
-
+    dp[i][0] = max(dp[i-1][1]+a, dp[i-1][2]+a)
+    dp[i][1] = max(dp[i-1][0]+b, dp[i-1][2]+b)
+    dp[i][2] = max(dp[i-1][0]+c, dp[i-1][1]+c)
+print(max(dp[N]))
