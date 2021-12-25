6e59da745da6efcd1ab0299630c11cb25505e183
chieping <takashi.honda.01@gmail.com>
2021-12-25 10:00:19 +0900

    typical90_002

--- typical90/002/main.py
+++ typical90/002/main.py
@@ -1,23 +1,24 @@
+from itertools import combinations
 N = int(input())
 
-def check(candidate):
-    dep = 0
-    for s in candidate:
+if N % 2:
+    print()
+    exit()
+
+def valid(S):
+    n = 0
+    for s in S:
         if s == '(':
-            dep += 1
+            n += 1
         else:
-            dep -= 1
-        if dep < 0:
+            n -= 1
+        if n < 0:
             return False
-    return dep == 0
+    return True
 
-
-for i in range(2**N):
-    candidate = ''
-    for j in range(N-1, -1, -1):
-        if i & (1<<j) == 0:
-            candidate += '('
-        else:
-            candidate += ')'
-    if check(candidate):
-        print(candidate)
+# "("の位置を示すposをcombinationsで作って回す
+for pos in combinations(range(N), N//2):
+    S = ['(' if p in pos else ')' for p in range(N)]
+    if not valid(S):
+        continue
+    print(''.join(S))
