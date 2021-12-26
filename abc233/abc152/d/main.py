9d8fb7fa90fcbee4b96c01b8a1443a71dadff0e3
chieping <takashi.honda.01@gmail.com>
2021-12-26 10:46:34 +0900

    abc152_d

--- abc233/abc152/d/main.py
+++ abc233/abc152/d/main.py
@@ -1,1 +1,9 @@
 N = int(input())
+cnt = [[0]*10 for _ in range(10)]
+for n in range(1, N+1):
+    cnt[int(str(n)[0])][n%10] += 1
+ans = 0
+for i in range(10):
+    for j in range(10):
+        ans += cnt[i][j] * cnt[j][i]
+print(ans)
