f53ef3b56b12a8267803d7b75d105653f46f1afb
chieping <takashi.honda.01@gmail.com>
2021-12-27 09:44:23 +0900

    update

--- typical90/014/main.py
+++ typical90/014/main.py
@@ -1,6 +1,7 @@
 N = int(input())
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
-A.sort()
-B.sort()
-print(sum(abs(A[i] - B[i]) for i in range(N)))
+A = sorted(list(map(int, input().split())))
+B = sorted(list(map(int, input().split())))
+ans = 0
+for i in range(N):
+    ans += abs(A[i]-B[i])
+print(ans)
