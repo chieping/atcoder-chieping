3161003caa7a6c1155fd339ac4b909d478351bb9
chieping <takashi.honda.01@gmail.com>
2022-01-07 08:40:55 +0900

    abc183_e

--- abc183/e/main.py
+++ abc183/e/main.py
@@ -1,33 +1,25 @@
-# TLE
-from pprint import pprint
 from sys import stdin
 H, W = map(int, stdin.readline().split())
 S = [list(stdin.readline()[:-1]) for _ in range(H)]
 MOD = 10**9+7
-dp = [[0] * (W+2) for _ in range(H+2)]
-dp[1][1] = 1
-for i in range(1, H+1):
-    for j in range(1, W+1):
-        if S[i-1][j-1] == '#':
+dp = [[0] * W for _ in range(H)]
+X = [[0] * W for _ in range(H)]
+Y = [[0] * W for _ in range(H)]
+Z = [[0] * W for _ in range(H)]
+
+dp[0][0] = 1
+for i in range(H):
+    for j in range(W):
+        if i == 0 and j == 0:
             continue
-        # 左から来た場合
-        k = i - 1
-        while k > 0 and S[k-1][j-1] != '#':
-            dp[i][j] += dp[k][j]
-            dp[i][j] %= MOD
-            k -= 1
-        # 上から来た場合
-        k = j - 1
-        while k > 0 and S[i-1][k-1] != '#':
-            dp[i][j] += dp[i][k]
-            dp[i][j] %= MOD
-            k -= 1
-        # 斜め上から来た場合
-        k = i - 1
-        l = j - 1
-        while k > 0 and l > 0 and S[k-1][l-1] != '#':
-            dp[i][j] += dp[k][l]
-            dp[i][j] %= MOD
-            k -= 1
-            l -= 1
-print(dp[H][W])
+        if S[i][j] == '#':
+            continue
+        if j > 0:
+            X[i][j] = (X[i][j-1] + dp[i][j-1]) % MOD
+        if i > 0:
+            Y[i][j] = (Y[i-1][j] + dp[i-1][j]) % MOD
+        if i > 0 and j > 0:
+            Z[i][j] = (Z[i-1][j-1] + dp[i-1][j-1]) % MOD
+        dp[i][j] = (X[i][j] + Y[i][j] + Z[i][j]) % MOD
+
+print(dp[H-1][W-1])
