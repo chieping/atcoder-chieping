b8bd3f977791c792139a5f419c65103cdc07b32c
chieping <takashi.honda.01@gmail.com>
2022-01-18 22:02:27 +0900

    mv

--- abc179/c/main.py
+++ abc179/c/main.py
@@ -1,9 +1,2 @@
 N = int(input())
-
-ans = 0
-
-for A in range(1, N):
-    b_cnt = (N-1) // A
-    ans += b_cnt
-
-print(ans)
+print(sum((N-1)//a for a in range(1, N)))
--- typical90/abc179/c/main.py
+++
@@ -1,2 +0,0 @@
-N = int(input())
-print(sum((N-1)//a for a in range(1, N)))
