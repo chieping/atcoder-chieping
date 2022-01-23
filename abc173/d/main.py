4ebe8ac2f57eb65904587103fd56d02f4b588ee2
chieping <takashi.honda.01@gmail.com>
2022-01-23 15:21:16 +0900

    update

--- abc173/d/main.py
+++ abc173/d/main.py
@@ -1,14 +1,8 @@
-import heapq
 import sys
 readline = sys.stdin.readline
 N = int(readline())
-A = list(map(int, readline().split()))
-A = [-a for a in A]
-A.sort()
-H = [A[0]]
-ans = 0
-for a in A[1:]:
-    ans += heapq.heappop(H)
-    heapq.heappush(H, a)
-    heapq.heappush(H, a)
-print(-ans)
+A = sorted(map(int, readline().split()), reverse=True)
+ans = -A[0]
+for i in range(N):
+    ans += A[i//2]
+print(ans)
