59d9b7913bcfc959cd81b5b2fc68e5cdb94069e1
chieping <takashi.honda.01@gmail.com>
2022-01-23 20:24:11 +0900

    abc178_d

--- abc178/d/main.py
+++ abc178/d/main.py
@@ -1,3 +1,7 @@
-from pprint import pprint
-import sys
-readline = sys.stdin.readline
+S = int(input())
+MOD = 10**9+7
+dp = [0] * (S+1)
+dp[0] = 1
+for i in range(3, S+1):
+    dp[i] = (dp[i-1] + dp[i-3]) % MOD
+print(dp[S])
