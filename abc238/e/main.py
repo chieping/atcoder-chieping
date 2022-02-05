3a2c5998e8a6626a76c81e99e11c3f8303faf821
chieping <takashi.honda.01@gmail.com>
2022-02-06 00:59:36 +0900

    abc238_e

--- abc238/e/main.py
+++ abc238/e/main.py
@@ -1,24 +1,30 @@
-from pprint import pprint
 import sys
 readline = sys.stdin.readline
 N, Q = map(int, readline().split())
 
-A = [0]*(N+1)
+parents = [-1] * (N+1)
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a == b:
+        return
+    parents[a] += parents[b]
+    parents[b] = a
+
+def find(a):
+    if parents[a] < 0:
+        return a
+    else:
+        parents[a] = find(parents[a])
+        return parents[a]
 
 for _ in range(Q):
     l, r = map(int, readline().split())
     l -= 1
-    A[l] += 1
-    A[r] -= 1
+    union(l, r)
 
-# print(A)
-for i in range(1, N):
-    A[i] += A[i-1]
-
-A = A[:N]
-# print(A)
-
-if min(A) == 0:
-    print('No')
-else:
+if find(0) == find(N):
     print('Yes')
+else:
+    print('No')
