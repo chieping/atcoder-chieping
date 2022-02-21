2b31f73f7617adf1e7e813fb506d659887a5e162
chieping <takashi.honda.01@gmail.com>
2022-02-21 23:14:13 +0900

    abc160_d

--- abc160/d/main.py
+++ abc160/d/main.py
@@ -1,12 +1,31 @@
-# WIP
 from collections import deque
-
-
 N, X, Y = map(int, input().split())
 X -= 1
 Y -= 1
 INF = 10**18
-ans = []
-for sv in range(N):
-    dist = [INF] * N
-    q = deque()
+ans = [0] * N
+
+for i in range(N):
+    Q = deque()
+    Q.append((i, 0))
+    dist = [INF]*N
+    dist[i] = 0
+    while len(Q)>0:
+        cur_v, cur_d = Q.pop()
+        nxtd = cur_d+1
+        if cur_v-1 >= 0 and nxtd < dist[cur_v-1] :
+            dist[cur_v-1] = nxtd
+            Q.append((cur_v-1, nxtd))
+        if cur_v+1 < N and nxtd < dist[cur_v+1]:
+            dist[cur_v+1] = nxtd
+            Q.append((cur_v+1, nxtd))
+        if X == cur_v and nxtd < dist[Y]:
+            dist[Y] = nxtd
+            Q.append((Y, nxtd))
+        if Y == cur_v and nxtd < dist[X]:
+            dist[X] = nxtd
+            Q.append((X, nxtd))
+    for d in dist:
+        ans[d] += 1
+ans = [a//2 for a in ans]
+print(*ans[1:], sep='\n')
