1874a8c596ef3c2acd22509753c40e721ba93615
chieping <takashi.honda.01@gmail.com>
2022-03-06 00:49:03 +0900

    abc242_d

--- abc242/d/main.py
+++ abc242/d/main.py
@@ -1,27 +1,26 @@
-from pprint import pprint
 import sys
 readline = sys.stdin.readline
 S = readline()[:-1]
-A = [ord(s) - 65 for s in list(S)] # A:0, B:1, C:2 に変換
-Slen = len(S)
 Q = int(readline())
 TK = [tuple(map(int, readline().split())) for _ in range(Q)]
-TK.sort()
 
-def to_c(i):
-    if i == 0:
-        return 'A'
-    elif i == 1:
-        return 'B'
-    else:
-        return 'C'
+def popcount(param):
+    n = param
+    c = 0
+    while n > 0:
+        n, r = divmod(n, 2)
+        c += r
+    return c
 
-curr = 0
-now = 2
 for t, k in TK:
     k -= 1
-    while t > 0:
-        d, r = divmod(k, 2)
-        t -= 1
-    
-
+    si = 0
+    if t <= 60:
+        b = 1<<t
+        si = k//b
+        k %= b
+    r = popcount(k)
+    l = t-r
+    x = l + r*2 + (ord(S[si])-65)
+    ans = chr(ord('A') + (x%3))
+    print(ans)
