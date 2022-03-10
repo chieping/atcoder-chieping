723feb6ad733db3442bf6d8911c7b2843f71bc4e
chieping <takashi.honda.01@gmail.com>
2022-03-10 23:07:49 +0900

    abc134_d

--- abc134/d/main.py
+++ abc134/d/main.py
@@ -1,3 +1,11 @@
-from pprint import pprint
-import sys
-readline = sys.stdin.readline
+N = int(input())
+A = [0] + list(map(int, input().split()))
+ans = [False] * (N+1)
+for i in range(N, 0, -1):
+    x = 0
+    for j in range(i, N+1, i):
+        x ^= ans[j]
+    ans[i] = A[i] ^ x
+ans = [i for i in range(1, N+1) if ans[i] == 1]
+print(len(ans))
+print(*ans)
