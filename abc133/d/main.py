bb8aba064268b4dd69687b580347cbe3bdbfb42b
chieping <takashi.honda.01@gmail.com>
2022-03-14 03:16:00 +0900

    abc133_d

--- abc133/d/main.py
+++ abc133/d/main.py
@@ -1,3 +1,16 @@
-from pprint import pprint
-import sys
-readline = sys.stdin.readline
+N = int(input())
+A = list(map(int, input().split()))
+Sum = sum(A)
+ans = [0] * N
+
+def solve(n):
+    ret = Sum
+    for i in range(n+1, n+N-1, 2):
+        ret -= 2*A[i%N]
+    return ret
+
+ans = [0] * N
+ans[0] = solve(0)
+for i in range(1, N):
+    ans[i] = (A[i-1] - ans[i-1]//2)*2
+print(*ans)
