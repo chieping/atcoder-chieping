0ba3609a995a189f04148c4d38f8e1b95787ea7b
chieping <takashi.honda.01@gmail.com>
2022-03-16 22:30:18 +0900

    abc168_d

--- abc168/d/main.py
+++ abc168/d/main.py
@@ -1,3 +1,21 @@
-from pprint import pprint
-import sys
-readline = sys.stdin.readline
+from collections import deque
+from sys import stdin
+input = stdin.readline
+N, M = map(int, input().split())
+edges = [[] for _ in range(N+1)]
+for i in range(M):
+    a, b = map(int, input().split())
+    edges[a].append(b)
+    edges[b].append(a)
+
+ans = [-1] * (N+1)
+Q = deque()
+Q.append(1)
+while len(Q) > 0:
+    now = Q.popleft()
+    for nxt in edges[now]:
+        if ans[nxt] == -1:
+            ans[nxt] = now
+            Q.append(nxt)
+print('Yes')
+print(*ans[2:], sep='\n')
