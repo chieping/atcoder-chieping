0dc34ea80f591846e802e3ce47d96e4e70148fbf
chieping <takashi.honda.01@gmail.com>
2022-03-19 18:28:35 +0900

    fix

--- src/combination.py
+++ src/combination.py
@@ -3,13 +3,13 @@ class Combination:
     def __init__(self, n: int, mod: int) -> None:
         assert n < mod
         self.mod = mod
-        self.fact = [0] * (N+1)
-        self.inv = [0] * (N+1)
-        self.fact_inv = [0] * (N+1)
+        self.fact = [0] * (n+1)
+        self.inv = [0] * (n+1)
+        self.fact_inv = [0] * (n+1)
         self.fact[0:2] = 1, 1
         self.fact_inv[0:2] = 1, 1
         self.inv[1] = 1
-        for i in range(2, N+1):
+        for i in range(2, n+1):
             self.fact[i] = self.fact[i-1] * i % self.mod
             self.inv[i] = self.mod - self.inv[self.mod%i] * (self.mod//i) % self.mod
             self.fact_inv[i] = self.fact_inv[i-1] * self.inv[i] % self.mod
