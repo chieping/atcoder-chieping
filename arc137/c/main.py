c083bf959d660d89c1dd37d5ac9df2651cf0f458
chieping <takashi.honda.01@gmail.com>
2022-03-20 09:13:03 +0900

    arc137_c

--- arc137/c/main.py
+++ arc137/c/main.py
@@ -1,5 +1,11 @@
-from pprint import pprint
 import sys
 input = sys.stdin.readline
 N = int(input())
 A = list(map(int, input().split()))
+if A[-1]-A[-2] > 1:
+    print('Alice')
+else:
+    if A[-1] % 2 == (N-1) % 2:
+        print('Bob')
+    else:
+        print('Alice')
