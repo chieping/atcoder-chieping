eccdd011ead1c0bcc7fb635202ba5daaaf1e078c
chieping <takashi.honda.01@gmail.com>
2022-03-21 19:49:20 +0900

    update

--- abc244/b/main.py
+++ abc244/b/main.py
@@ -1,14 +1,14 @@
-import sys
-input = sys.stdin.readline
 N = int(input())
-T = list(input()[:-1])
-now = [0, 0]
-dil = [(1, 0), (0, -1), (-1, 0), (0, 1)]
-di = 0
+T = list(input())
+dx = [1, 0, -1, 0]
+dy = [0, -1, 0, 1]
+d = 0
+x = 0
+y = 0
 for c in T:
     if c == 'S':
-        now[0] += dil[di][0]
-        now[1] += dil[di][1]
+        x += dx[d]
+        y += dy[d]
     else:
-        di = (di + 1) % 4
-print(*now)
+        d = (d + 1) % 4
+print(x, y)
--- abc244/f/main.py
+++ abc244/f/main.py
@@ -1,12 +1,36 @@
-from itertools import product
-from pprint import pprint
+from collections import deque
 import sys
 input = sys.stdin.readline
 N, M = map(int, input().split())
 edge = [[] for _ in range(N)]
 for _ in range(M):
     a, b = map(int, input().split())
-    a -= 1
-    b -= 1
+    a -= 1; b -= 1
     edge[a].append(b)
     edge[b].append(a)
+
+# 文字列Sに関する良いパス、かつ最後がlstのパス
+# のうち最短のものの長さ
+opt = [[0] * 17 for _ in [0] * (1<<17)]
+que = deque()
+
+for i in range(N):
+    opt[1<<i][i] = 1
+    que.append((1<<i, i))
+
+while len(que) > 0:
+    msk, lst = que.popleft()
+    for to in edge[lst]:
+        msk2 = msk ^ (1<<to)
+        if opt[msk2][to] == 0:
+            opt[msk2][to] = opt[msk][lst] + 1
+            que.append((msk2, to))
+
+ans = 0
+for msk in range(1, 1<<N):
+    mi = 10**18
+    for lst in range(N):
+        mi = min(mi, opt[msk][lst])
+    ans += mi
+
+print(ans)
