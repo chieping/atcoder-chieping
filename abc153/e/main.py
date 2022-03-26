bdaf8a9aa32513a918c745096c6c4c68206445f0
chieping <takashi.honda.01@gmail.com>
2022-03-26 12:17:46 +0900

    abc153_e

--- abc153/e/main.py
+++ abc153/e/main.py
@@ -1,3 +1,14 @@
-from pprint import pprint
 import sys
 readline = sys.stdin.readline
+HP, N = map(int, input().split())
+AB = [list(map(int, input().split())) for _ in range(N)]
+INF = 10**18
+dp = [INF] * (20010)
+dp[0] = 0
+for i in range(20005):
+    next_dp = dp.copy()
+    for a, b in AB:
+        if i-a >= 0 and dp[i-a] != INF:
+            next_dp[i] = min(next_dp[i], dp[i-a] + b)
+    dp = next_dp
+print(min(dp[HP:]))
