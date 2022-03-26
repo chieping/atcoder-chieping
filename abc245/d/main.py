ed2e891cee6e883231f96fcef52b5dca36d29e52
chieping <takashi.honda.01@gmail.com>
2022-03-27 00:09:03 +0900

    abc245_d

--- abc245/d/main.py
+++ abc245/d/main.py
@@ -3,3 +3,11 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
 C = list(map(int, input().split()))
+B = [0] * (M+1)
+
+for i in range(M, -1, -1):
+    B[i] = C[N+i]//A[N]
+    for j in range(N+1):
+        C[i+j] -= A[j] * B[i]
+
+print(*B)
