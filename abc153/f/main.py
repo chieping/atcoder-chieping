9b9bc2435ed78e49cd026f25cade1c2b18e15e49
chieping <takashi.honda.01@gmail.com>
2022-03-26 13:34:56 +0900

    abc153_f

--- abc153/f/main.py
+++ abc153/f/main.py
@@ -1,3 +1,19 @@
-from pprint import pprint
+from collections import deque
 import sys
 readline = sys.stdin.readline
+N, D, A = map(int, input().split())
+XH = [list(map(int, input().split())) for _ in range(N)]
+XH.sort()
+ans = 0
+q = deque()
+tot = 0
+for x, h in XH:
+    while len(q) and q[0][0] < x:
+        tot -= q.popleft()[1]
+    h -= tot
+    if h > 0:
+        n = (h+A-1)//A
+        ans += n
+        tot += n*A
+        q.append((x+2*D, n*A))
+print(ans)
