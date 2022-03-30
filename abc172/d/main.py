666b0223c57fb39994e0c3a7523646f56a0f5bc1
chieping <takashi.honda.01@gmail.com>
2022-03-31 00:11:48 +0900

    abc172_d

--- abc172/d/main.py
+++ abc172/d/main.py
@@ -1,3 +1,8 @@
-from pprint import pprint
-import sys
-readline = sys.stdin.readline
+# 解説AC
+N = int(input())
+ans = 0
+def f(n):
+    return n*(n+1)//2
+for i in range(1, N+1):
+    ans += i * f(N//i)
+print(ans)
