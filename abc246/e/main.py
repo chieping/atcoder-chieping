a60437d18d5b24a2fa2920b2796552b8ac94d60f
chieping <takashi.honda.01@gmail.com>
2022-04-03 13:34:09 +0900

    abc246_e

--- abc246/e/main.py
+++ abc246/e/main.py
@@ -1,22 +1,19 @@
-# TLE
 from collections import deque
-from pprint import pprint
 import sys
 input = sys.stdin.readline
 N = int(input())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
-A = [a-1 for a in A]
-B = [b-1 for b in B]
-INF = 10**18
+INF = 10**9
 dxs = [-1, -1, 1, 1]
 dys = [1, -1, -1, 1]
-
-board = [list(input()[:-1]) for _ in range(N)]
-dist = [[INF] * N for _ in range(N)]
-
+board = []
+board.append(['#'] * (N+2))
+for _ in range(N):
+    board.append(['#'] + list(input()[:-1]) + ['#'])
+board.append(['#'] * (N+2))
+dist = [[INF] * (N+2) for _ in range(N+2)]
 dist[A[0]][A[1]] = 0
-
 q = deque()
 q.append(A)
 
@@ -24,16 +21,14 @@ while len(q):
     i, j = q.popleft()
     cnt = dist[i][j]
     for dx, dy in zip(dxs, dys):
-        ci = i
-        cj = j
-        while 0 <= ci < N and 0 <= cj < N and board[ci][cj] != '#':
+        ci = i + dx
+        cj = j + dy
+        while board[ci][cj] != '#' and dist[ci][cj] > dist[i][j]:
             if cnt + 1 < dist[ci][cj]:
                 dist[ci][cj] = cnt+1
                 q.append((ci, cj))
             ci += dx
             cj += dy
 
-if dist[B[0]][B[1]] == INF:
-    print(-1)
-else:
-    print(dist[B[0]][B[1]])
+ans = dist[B[0]][B[1]]
+print(ans if ans != INF else -1)
