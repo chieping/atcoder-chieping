2ed23ea408f8c4cd6130e004f2273b48044287ff
chieping <takashi.honda.01@gmail.com>
2022-04-09 12:42:14 +0900

    arc128_b

--- arc128/b/main.py
+++ arc128/b/main.py
@@ -1,3 +1,13 @@
-from pprint import pprint
 import sys
 input = sys.stdin.readline
+T = int(input())
+C = [list(map(int, input().split())) for _ in range(T)]
+INF = 10**18
+for c in C:
+    ans = INF
+    for i in range(3):
+        for j in range(i+1, 3):
+            if (c[i]-c[j]) % 3:
+                continue
+            ans = min(ans, max(c[i], c[j]))
+    print(ans if ans != INF else -1)
