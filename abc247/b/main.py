1a33bad3f4b9d1470f181b1f4a214c36ca97f1c8
chieping <takashi.honda.01@gmail.com>
2022-04-11 00:18:38 +0900

    update

--- abc247/b/main.py
+++ abc247/b/main.py
@@ -1,5 +1,3 @@
-from collections import Counter
-from pprint import pprint
 import sys
 input = sys.stdin.readline
 N  = int(input())
@@ -9,43 +7,18 @@ for _ in range(N):
     s, t = input().split()
     S.append(s)
     T.append(t)
- 
-SC = Counter(S)
-ST = Counter(T)
- 
-ng = set()
-for s, t in zip(S, T):
-    if s == t:
-        if s in ng:
-            print('No')
-            exit()
-        else:
-            ng.add(s)
-            continue
- 
-    if SC[s] + ST[s] == 1 or SC[t] + ST[t] == 1:
-        if s in ng or t in ng:
-            print('No')
-            exit()
-        else:
-            continue
-        
-    if SC[s] + ST[s] > 1 and SC[t] + ST[t] > 1:
+
+for i in range(N):
+    temp1 = 0
+    temp2 = 0
+    for j in range(N):
+        if i != j:
+            if S[i] == S[j] or S[i] == T[j]:
+                temp1 = 1
+            if T[i] == T[j] or T[i] == S[j]:
+                temp2 = 1
+    if temp1 == 1 and temp2 == 1:
         print('No')
         exit()
-    if SC[s] + ST[s] > 1:
-        if t not in ng:
-            ng.add(t)
-            continue
-        else:
-            print('No')
-            exit()
-    if SC[t] + ST[t] > 1:
-        if s not in ng:
-            ng.add(s)
-            continue
-        else:
-            print('No')
-            exit()
- 
-print('Yes')
+
+print('Yes') 
--- abc247/d/main.py
+++ abc247/d/main.py
@@ -1,28 +1,24 @@
 from collections import deque
 import sys
 input = sys.stdin.readline
-
 que = deque()
-
 Q = int(input())
 for _ in range(Q):
     q = list(map(int, input().split()))
-
     if q[0] == 1:
         x, c = q[1:]
         que.append((x, c))
     else:
-        c = q[1]
+        rem = q[1]
         Sum = 0
-        while c > 0:
+        while rem > 0:
             X, C = que.popleft()
-            if c < C:
-                nc = C-c
+            if rem < C:
+                nc = C-rem
                 que.appendleft((X, nc))
-                Sum += X*c
-                c = 0
+                Sum += X*rem
+                rem = 0
             else:
-                c -= C
+                rem -= C
                 Sum += X*C
         print(Sum)
-
