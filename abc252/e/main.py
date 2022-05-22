71e9443dec29bc9e31bbb3c175dc4000fe084ec3
chieping <takashi.honda.01@gmail.com>
2022-05-23 00:13:17 +0900

    abc252_e

--- abc252/e/main.py
+++ abc252/e/main.py
@@ -1,39 +1,28 @@
-# WA
 from heapq import heappop, heappush
 N, M = map(int, input().split())
 edge = [[] for _ in range(N)]
 for i in range(1, M+1):
     a, b, c = map(int, input().split())
     a -= 1; b -= 1
-    edge[a].append((c, b, i))
-    edge[b].append((c, a, i))
+    # to, cost, edge_id
+    edge[a].append((b, c, i))
+    edge[b].append((a, c, i))
 
-ans = set()
-
-def dijkstra(v):
-    Q = []
-    dist = [-1] * N
-    done = [False] * N
-    dist[0] = 0
-    done[0] = True
-    for c, j, i in edge[0]:
-        heappush(Q, (c, j, i))
-    
-    while len(Q):
-        c, j, i = heappop(Q)
-
-        if done[j]:
+INF = 10**18
+ans = [-1] * N
+dist = [INF] * N
+dist[0] = 0
+Q = []
+heappush(Q, (0, 0))
+while len(Q):
+    d, v = heappop(Q)
+    if dist[v] != d:
+        continue
+    for u, cost, i in edge[v]:
+        now_d = d + cost
+        if dist[u] <= now_d:
             continue
-        done[j] = True
-
-        ans.add(i)
-
-        for cc, jj, ii in edge[j]:
-            if dist[jj] == -1 or dist[jj] > dist[j] + cc:
-                dist[jj] = dist[j] + cc
-                heappush(Q, (dist[jj], jj, ii))
-
-for v in range(1, N):
-    dijkstra(v)
-
-print(*ans)
+        dist[u] = now_d
+        ans[u] = i
+        heappush(Q, (now_d, u))
+print(*ans[1:])
