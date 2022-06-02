beb56d96946661f8a2fb42792b4d1e45f4dbeac8
chieping <takashi.honda.01@gmail.com>
2022-06-02 22:49:22 +0900

    abc170_d

--- abc170/d/main.py
+++ abc170/d/main.py
@@ -1,3 +1,15 @@
-from pprint import pprint
-import sys
-readline = sys.stdin.readline
+from collections import Counter
+N = int(input())
+A = list(map(int, input().split()))
+B = set(k for k, v in Counter(A).items() if v == 1)
+A.sort()
+Max = A[-1]
+C = set()
+
+for a in A:
+    j = 2
+    while a*j <= Max:
+        C.add(a*j)
+        j += 1
+
+print(len(B - C))
