415cb45bfce4b6adb42fc9a2da4b0907ce65e76a
chieping <takashi.honda.01@gmail.com>
2022-06-04 10:20:42 +0900

    abc178_e

--- abc178/e/main.py
+++ abc178/e/main.py
@@ -1,3 +1,14 @@
-from pprint import pprint
 import sys
-readline = sys.stdin.readline
+input = sys.stdin.readline
+N = int(input())
+X = [0] * N
+Y = [0] * N
+for i in range(N):
+    X[i], Y[i] = map(int, input().split())
+
+# マンハッタン距離は45度回転
+XX = [x-y for x, y in zip(X, Y)]
+YY = [x+y for x, y in zip(X, Y)]
+
+ans = max(abs(max(XX)-min(XX)), abs(max(YY)-min(YY)))
+print(ans)
