e89ab12117844cae4dee13f01fe3833239b35143
chieping <takashi.honda.01@gmail.com>
2022-06-12 18:26:37 +0900

    abc154_e

--- abc154/e/main.py
+++ abc154/e/main.py
@@ -1,3 +1,27 @@
-from pprint import pprint
+# 解説AC
+from functools import lru_cache
 import sys
-readline = sys.stdin.readline
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+N = int(input())
+K = int(input())
+
+@lru_cache(maxsize=None)
+def f(n, k):
+    assert n >= 0
+    if n < 10:
+        if k == 0:
+            return 1
+        if k == 1:
+            return n
+        return 0
+    q, r = divmod(n, 10)
+    ret = 0
+    if k >= 1:
+        ret += f(q, k-1) * r
+        ret += f(q-1, k-1) * (9-r)
+    ret += f(q, k)
+    return ret
+
+print(f(N, K))
