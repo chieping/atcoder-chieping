78afac99f025fc5a0f4664e02d425f06884879fb
chieping <takashi.honda.01@gmail.com>
2022-06-27 07:31:38 +0900

    abc257_d

--- abc257/d/main.py
+++ abc257/d/main.py
@@ -7,29 +7,12 @@ for _ in range(N):
     X.append(x)
     Y.append(y)
     P.append(p)
-INF = float('inf')
-
-ng = 0
-ok = 10**10
-
-while abs(ng-ok) > 1:
-    mid = (ng+ok)//2
-    d = [[0] * N for _ in range(N)]
+S = [[0] * N for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        S[i][j] = (abs(X[i]-X[j])+abs(Y[i]-Y[j])+P[i]-1)//P[i]
+for k in range(N):
     for i in range(N):
         for j in range(N):
-            dist = abs(X[i]-X[j]) + abs(Y[i]-Y[j])
-            if mid*P[i] >= dist:
-                d[i][j] = 1
-    for k in range(N):
-        for i in range(N):
-            for j in range(N):
-                d[i][j] |= d[i][k] and d[k][j]
-    for i in range(N):
-        if all(d[i]):
-            # print('ok', mid)
-            ok = mid
-            break
-    else:
-        # print('ng', mid)
-        ng = mid
-print(ok)
+            S[i][j] = min(S[i][j], max(S[i][k], S[k][j]))
+print(min(max(S[i]) for i in range(N)))
