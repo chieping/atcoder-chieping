3b1d4bbbe9e9c28ef3b1a83d5512c3eff27955da
chieping <takashi.honda.01@gmail.com>
2022-06-26 23:21:31 +0900

    abc257_d

--- abc257/d/main.py
+++ abc257/d/main.py
@@ -1,38 +1,35 @@
 import sys
 input = sys.stdin.readline
 N = int(input())
-X = []
-Y = []
-P = []
+X, Y, P = [], [], []
 for _ in range(N):
     x, y, p = map(int, input().split())
     X.append(x)
     Y.append(y)
     P.append(p)
+INF = float('inf')
 
-INF = 10**18
-dist = [[INF] * N for _ in range(N)]
-for i in range(N):
-    dist[i][i] = 0
+ng = 0
+ok = 10**10
 
-# for k in range(N):
-#     for x in range(N):
-#         for y in range(N):
-#             dist[x][y] = min(dist[x][y], dist[x][k] + dist[k][y])
-
-# print(dist)
-
-ans = 10**18
-
-for i in range(N):
-    for j in range(N):
-        if i == j: dist[i][j] = 0
-        d = abs(X[i]-X[j]) + abs(Y[i]-Y[j])
-        dist[i][j] = (d+P[i]-1)//P[i]
-
-for k in range(N):
-    for x in range(N):
-        for y in range(N):
-            dist[x][y] = max(dist[x][y], dist[x][k] + dist[k][y])
-print(dist)
-print(ans)
+while abs(ng-ok) > 1:
+    mid = (ng+ok)//2
+    d = [[0] * N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            dist = abs(X[i]-X[j]) + abs(Y[i]-Y[j])
+            if mid*P[i] >= dist:
+                d[i][j] = 1
+    for k in range(N):
+        for i in range(N):
+            for j in range(N):
+                d[i][j] |= d[i][k] and d[k][j]
+    for i in range(N):
+        if all(d[i]):
+            # print('ok', mid)
+            ok = mid
+            break
+    else:
+        # print('ng', mid)
+        ng = mid
+print(ok)
