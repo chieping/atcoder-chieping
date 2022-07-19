5c4f77cc7e5b6daab101f4aff64b2f21b480a7df
chieping <takashi.honda.01@gmail.com>
2022-07-19 23:58:58 +0900

    abc164_d

--- abc164/d/main.py
+++ abc164/d/main.py
@@ -1,3 +1,12 @@
-from pprint import pprint
-import sys
-readline = sys.stdin.readline
+S = list(map(int, input()))
+l = len(S)
+dp = [0] * 2020
+ans = 0
+for i, s in enumerate(S, 1):
+    prev = dp
+    dp = [0] * 2020
+    dp[s] = 1
+    for j in range(2020):
+        dp[(j*10+s)%2019] += prev[j]
+    ans += dp[0]
+print(ans)
