16eef1f317c8ac209d504e86bcabfe38c985236f
chieping <takashi.honda.01@gmail.com>
2022-07-20 07:43:48 +0900

    abc239_e

--- abc239/e/main.py
+++ abc239/e/main.py
@@ -1,51 +1,35 @@
-# WA
-import heapq
-from pprint import pprint
 import sys
-readline = sys.stdin.readline
-
-N, Q = map(int, readline().split())
-X = list(map(int, readline().split()))
-
-class Node:
-    def __init__(self, key):
-        self.data = key
-        self.left = None
-        self.right = None
-
-    def nums(self):
-        res = [self.data]
-        if self.left:
-            res = list(heapq.merge(res, self.left.nums()))
-        if self.right:
-            res = list(heapq.merge(res, self.right.nums()))
-        return res
-
-nodes = [Node(x) for x in X]
-edges = [[] for _ in range(N)]
-
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+N, Q = map(int, input().split())
+X = list(map(int, input().split()))
+edge = [[] for _ in range(N)]
 for _ in range(N-1):
-    a, b = map(int, readline().split())
-    a -= 1
-    b -= 1
-    edges[a].append(b)
-    edges[b].append(a)
-
-def dfs(n, ex):
-    for v in edges[n]:
-        if v == ex:
-            continue
-        if not nodes[n].left:
-            nodes[n].left = nodes[v]
-        else:
-            nodes[n].right = nodes[v]
-        dfs(v, n)
-
-dfs(0, 0)
-
+    a, b = map(int, input().split())
+    a -= 1; b -= 1
+    edge[a].append(b)
+    edge[b].append(a)
+V = []
+K = []
 for _ in range(Q):
-    v, k = map(int, readline().split())
+    v, k = map(int, input().split())
     v -= 1
     k -= 1
-    nums = nodes[v].nums()
-    print(nums[len(nums)-1-k])
+    V.append(v)
+    K.append(k)
+
+M = [[] for _ in range(N)]
+
+def dfs(v, p = -1):
+    M[v].append(X[v])
+    for u in edge[v]:
+        if u == p: continue
+        dfs(u, v)
+        M[v] += M[u]
+        M[v].sort(reverse=True)
+        M[v] = M[v][:20]
+
+dfs(0)
+
+for v, k in zip(V, K):
+    print(M[v][k])
